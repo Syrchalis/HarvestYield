3589be568c34606c4fbbf962e1a18a216d47c2c1
Syrchalis <41159904+Syrchalis@users.noreply.github.com>
2018-08-01 20:51:20 +0200

    Add files via upload

---
+++ Source/HarmonyPatches.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
+
+namespace HarvestYieldPatch
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        // this static constructor runs to create a HarmonyInstance and install a patch.
+        static HarmonyPatches()
+        {
+            var harmony = HarmonyInstance.Create("syrchalis.rimworld.harvestYieldPatch");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Log.Message("[HarvestYieldPatch] loaded...");
+        }
+    }
+}
---
+++ Source/HarvestYieldPatch.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Harmony;
+using RimWorld;
+using Verse;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace HarvestYieldPatch
+{ 
+    [HarmonyPatch]
+    public static class HarvestYieldPatch
+    {
+        public static MethodInfo match = typeof(Plant).GetMethod("YieldNow");
+        public static MethodInfo replaceWith = typeof(HarvestYieldPatch).GetMethod("YieldNowPatch");
+        public static MethodInfo TargetMethod()
+        {
+            Type mainType = typeof(JobDriver_PlantWork);
+            Log.Message("TargetMethod: Main Type Found");
+            Type iteratorType = mainType.GetNestedTypes(BindingFlags.NonPublic | BindingFlags.Instance).First(t => t.FullName.Contains("c_Iterator"));
+            Log.Message("TargetMethod: Iterator Type Resolved");
+            Type anonStoreyType = iteratorType.GetNestedTypes(BindingFlags.NonPublic | BindingFlags.Instance).First(t => t.FullName.Contains("c_AnonStorey"));
+            Log.Message("TargetMethod: AnonStorey Type Resolved");
+            return anonStoreyType.GetMethods().First(m => m.ReturnType == typeof(void));
+        }
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
+        {
+            foreach (CodeInstruction i in instr)
+            {
+                if (i.operand == match)
+                {
+                    Log.Message("Instruction insertion complete!");
+                    yield return new CodeInstruction(OpCodes.Ldloc_0);
+                    yield return new CodeInstruction(OpCodes.Call, replaceWith);
+                }
+                else
+                {
+                    yield return i;
+                }
+            }
+        }
+        public static int YieldNowPatch(Plant p, Pawn actor)
+        {
+            float harvestFactor = actor.GetStatValue(StatDefOf.PlantHarvestYield, true);
+            float harvestAmount = p.YieldNow() * harvestFactor;
+
+            if (harvestFactor < 1)
+            {
+                Log.Message("harvest Factor <1");
+                return p.YieldNow();
+            }
+            else
+            {
+                Log.Message("harvest Factor >1");
+                return GenMath.RoundRandom(harvestAmount);
+            };
+        }
+    }
+}
